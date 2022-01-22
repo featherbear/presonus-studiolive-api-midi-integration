c28012fa44a9aec41b0d8b1794c40b85e6cdc903
Andrew Wong <andrew.j.wong@outlook.com>
2022-01-23 03:57:53 +1100

    Add config structure
    `<F extends unknown>(fn: (client: API) => F) => F` is magical.
    Only took me like 40 minutes to figure it out :///

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import easymidi from 'easymidi'
 import * as bunyan from 'bunyan'
 import API from 'presonus-studiolive-api-simple-api'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 declare global {
     var logger: bunyan
 }
@@ -10,27 +13,71 @@ globalThis.logger = bunyan.createLogger({
     name: "PreSonus StudioLive MIDI Integration"
 })
 
+const { HOST, PORT } = process.env
+if (!HOST) {
+    logger.error("No HOST env set. Exiting")
+}
+
 logger.info("Starting")
+
+import config, { NoteAction } from './config'
+
+let noteLatches = {}
+
+function handleNoteAction(action: NoteAction, noteConfig: typeof config['notes'][string]) {
+    if (!action) return
+
+    switch (action) {
+        case 'mute': {
+            return withClient(client => client.mute(noteConfig.selector, true))
+        }
+        case 'unmute': {
+            return withClient(client => client.mute(noteConfig.selector, false))
+        }
+    }
+}
 
 const inputDevices = easymidi.getInputs()
 inputDevices.forEach(deviceName => {
     logger.info({ deviceName }, "Found MIDI input")
     let inputDevice = new easymidi.Input(deviceName)
+
+    inputDevice.on('noteon', function (data) {
+        let note = data.note.toString()
+
+        let noteConfig = config.notes[note]
+        if (!noteConfig) return
+
+        if (noteConfig.latch && noteLatches[note]) {
+            noteLatches[note] = false
+            handleNoteAction(noteConfig.onRelease, noteConfig)
+        } else {
+            if (noteConfig.latch) noteLatches[note] = true
+            handleNoteAction(noteConfig.onPress, noteConfig)
+        }
+    })
+
+    inputDevice.on('noteoff', function (data) {
+        let note = data.note.toString()
+
+        let noteConfig = config.notes[note]
+        if (!noteConfig) return
+        if (noteConfig.latch) return
+
+        handleNoteAction(noteConfig.onRelease, noteConfig)
+    })
+
     inputDevice.on('cc', function (data) {
+        let controller = data.controller.toString()
 
-        // For now, only handle channels 1 - 9
-        if (data.controller <= 0 || data.controller > 9) return
+        let controllerConfig = config.controllers[controller]
+        if (!controllerConfig) return
 
-        let value = Math.trunc(data.value / 127 * 100)
-        logger.debug(
-            {
-                ...data,
-                controller: deviceName,
-                value
-            },
-            "Got control change"
-        )
-        sendValue(value, data.controller)
+        switch (controllerConfig.type) {
+            case 'volume': {
+                withClient(client => client.setLevel(controllerConfig.selector, Math.trunc(data.value / 127 * 100)))
+            }
+        }
     })
 
     let closeCheckTimer =
@@ -42,14 +89,13 @@ inputDevices.forEach(deviceName => {
         }, 2000)
 })
 
-let sendValue: (value: number, channel: number) => any = () => { }
+let withClient: <F extends unknown>(fn: (client: API) => F) => F = () => {
+    logger.warn("Dropping client command as the console was not yet connected")
+    return null;
+}
 
-const HOST = "192.168.0.21"
 logger.info({ host: HOST }, "Connecting to StudioLive console")
-let client = new API(HOST)
-client.connect({ clientName: "MIDI Integration" }).then(() => {
+new API(HOST).connect({ clientName: "MIDI Integration" }).then((client) => {
     logger.info("Successfully connected to StudioLive console")
-    sendValue = (value: number, channel: number) => {
-        client.setLevel({ type: "LINE", channel: channel }, value)
-    }
+    withClient = (fn) => fn(client)
 })
