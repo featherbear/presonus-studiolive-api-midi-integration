6d4d54e54b31ffbbe6cb38dc3556c55d79940fd0
Andrew Wong <andrew.j.wong@outlook.com>
2022-01-23 19:36:54 +1100

    Add fallback values

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs'
+import { readFileSync, writeFileSync, existsSync } from 'fs'
 import { join } from 'path'
 import type { ChannelSelector } from 'presonus-studiolive-api-simple-api'
 import { ControllerType, NoteAction } from './types/configTypes'
@@ -20,9 +20,13 @@ const store: {
             selector: ChannelSelector
         }
     }
-} = JSON.parse(readFileSync(configPath).toString('utf8'))
+} = (existsSync(configPath) && JSON.parse(readFileSync(configPath).toString('utf8')))
+    || { controllers: {}, notes: {} }
+
 
 export default store
 export function updateStore() {
     writeFileSync(configPath, JSON.stringify(store, null, 4), 'utf8')
 }
+
+console.log(store);
