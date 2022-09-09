06342dbd3943651e4e3be42bccef9d5652816a69
Andrew Wong <andrew.j.wong@outlook.com>
2022-09-09 23:48:30 +1000

    Cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script>
-	import successkid from 'images/successkid.jpg';
 </script>
 
 <style>
@@ -40,11 +39,3 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
