77cfa681c97cfa10894906e6203c314df49e3a6b
Andrew Wong <andrew.j.wong@outlook.com>
2025-08-22 00:40:27 +1000

    Console and MIDI Controller API reads

--- src/lib/rpc/contract.ts
+++ src/lib/rpc/contract.ts
@@ -1,7 +1,8 @@
-import MidiConnection from "$lib/MidiConnection";
-
 import { z } from "zod";
 import { oc } from "@orpc/contract";
+import { MidiConnectionInterop } from "$lib/types/MidiConnectionInterop";
+import { ConsoleConnectionInterop } from "$lib/types/ConsoleConnectionInterop";
+import { MidiControllerInterop, MidiControllerInteropNoConfig } from "$lib/types/MidiControllerInterop";
 
 export const contract = {
   midi: oc.prefix("/midi").router({
@@ -17,11 +18,13 @@ export const contract = {
         })
       ),
 
-    getMidiConnections: oc.route({
-      method: "GET",
-      path: "/connection",
-    }),
-    // .output(z.object({}))
+    getMidiConnections: oc
+      .route({
+        method: "GET",
+        path: "/connection",
+      })
+      .output(z.array(MidiConnectionInterop)),
+
     getMidiConnection: oc
       .route({
         method: "GET",
@@ -31,13 +34,49 @@ export const contract = {
         z.object({
           id: z.string().describe("Connection"),
         })
-      ),
-    // .output(z.object({}))
+      )
+      .output(MidiConnectionInterop),
+
+    getMidiControllers: oc
+      .route({
+        method: "GET",
+        path: "/controller",
+      })
+      .output(z.array(MidiControllerInteropNoConfig)),
+    getMidiController: oc
+      .route({
+        method: "GET",
+        path: "/controller/{id}",
+      })
+      .input(
+        z.object({
+          id: z.string().describe("Controller"),
+        })
+      )
+      .output(MidiControllerInterop),
   }),
   console: oc.prefix("/console").router({
-    discover: oc.route({
-      method: "GET",
-      path: "/discover",
-    }),
+    discover: oc
+      .route({
+        method: "GET",
+        path: "/discover",
+      })
+      .output(
+        z.array(
+          z.object({
+            name: z.string(),
+            serial: z.string(),
+            ip: z.string(),
+            port: z.number().min(1).max(65535),
+            timestamp: z.coerce.date(),
+          })
+        )
+      ),
+    getConsoleConnections: oc
+      .route({
+        method: "GET",
+        path: "/connections",
+      })
+      .output(z.array(ConsoleConnectionInterop)),
   }),
 };
